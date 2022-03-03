6aa982a53fb795fb617d94daeac8211b0b71c62a
Mishihiro Kawaji <michikun06@icloud.com>
2022-03-03 14:18:35 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,31 @@
 class UsersController < ApplicationController
-
-  def new
+  
+  # Get /users/:id/show
+  def show
+    @user = User.find(params[:id])
   end
+  
+  # Get /users/new
+  def new
+    @user = User.new
+  end
+  
+  # Post /users/new
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功をここで扱う。
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
